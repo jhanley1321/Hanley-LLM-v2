f5067dd874b0b3639dfd4ba0480f0bbb4efc0702
James <41175538+jhanley1321@users.noreply.github.com>
2025-10-16 00:38:54 -0500

    REFACTOR

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from vector_store import LangChainVectorStore
+from rag.vector_store import LangChainVectorStore
 from rag.rag import RAGPipeline
 from llm import LLM
 from rag.rag_chat_handler import RAGChatHandler
@@ -6,23 +6,21 @@ from ui import ChatUI
 
 
 
-##### Move Logic Else where #######
-# Initialize components
-vs = LangChainVectorStore(collection_name="restaurant_reviews")
-llm = LLM(model="llama2")
-rag = RAGPipeline(vector_store=vs, csv_path="data/realistic_restaurant_reviews.csv")
 
-# Build index (reuse or rebuild depending on flag)
-rag.build_index(force_rebuild=False)
 
-# Wrap LLM + RAG into chat handler
-rag_chat = RAGChatHandler(rag_pipeline=rag, llm=llm)
-##################################
 
 
 def main():
 
+    vs = LangChainVectorStore(collection_name="restaurant_reviews")
+    llm = LLM(model="llama2")
+    rag = RAGPipeline(vector_store=vs, csv_path="data/realistic_restaurant_reviews.csv")
 
+    # Build index (reuse or rebuild depending on flag)
+    rag.build_index(force_rebuild=False)
+
+    # Wrap LLM + RAG into chat handler
+    rag_chat = RAGChatHandler(rag_pipeline=rag, llm=llm)
     # Run Streamlit UI
     ChatUI(chat_handler=rag_chat).run()
 
